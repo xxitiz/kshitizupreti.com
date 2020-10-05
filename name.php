c8127561bad57fca1d15079a56c2d5c132a63eff
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2020-10-05 10:01:47 +0545

    Update name.php
    Time Zone for Nepal added

--- name.php
+++ name.php
@@ -1,7 +1,8 @@
 <?php
 $name=$_GET['n'];
 $myfile = fopen("names.html", "a") or die("Unable to open file!");
-$txt = $name . " = " . date("Y/m/d") . " on " . date("l") . " at " . date("h:i:s a") . "\n <br> \n";
+date_default_timezone_set("Asia/Kathmandu");
+$txt = $name . " = " . date("Y/m/d") . " on " . date("l") . " at " . date("h:i:s a") . "\n <br>\n";
 fwrite($myfile, $txt);
 fclose($myfile);
 ?>
